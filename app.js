31bf384c93c362b6f967f432ba1c3e5bf06aee7b
pastaBoi07 <46807055+pastaBoi07@users.noreply.github.com>
2023-10-08 15:21:31 -0400

    replaced console.logs with debugs

--- app.js
+++ app.js
@@ -1,64 +1,63 @@
-const express = require('express');
-const dotenv = require('dotenv').config();
-const debug = require("debug")("test-debug");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const debug = require("debug")("test");
 
-const errorHandler = require('./middleware/errorMiddleware');
-const connectDB =require('./config/db');
+const errorHandler = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db");
 const port = process.env.PORT || 6000;
-const path = require('path');
-const logger = require('morgan');
-const cookieParser = require('cookie-parser');
+const path = require("path");
+const logger = require("morgan");
+const cookieParser = require("cookie-parser");
 
-const passport = require('passport');
-const session = require('express-session');
+const passport = require("passport");
+const session = require("express-session");
 
-const indexRoutes = require('./routes/index');
-const userRoutes = require('./routes/userRoutes');
-const messageRoutes = require('./routes/messageRoutes');
+const indexRoutes = require("./routes/index");
+const userRoutes = require("./routes/userRoutes");
+const messageRoutes = require("./routes/messageRoutes");
 
 connectDB();
+debug("MONGODB is set");
 
 const app = express();
 
-app.set('views', path.join(__dirname,'views'))
-app.set('view engine','pug');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 
 //authenticaion
 
-app.use(session({
-  secret: process.env.SESSION_SECRET, 
-  resave: false, 
-  saveUninitialized: true}
-));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 
 //req header parsing
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use((req,res,next)=>{
-  console.log(req.user);
-  console.log(req.isAuthenticated());
+app.use((req, res, next) => {
   res.locals.currentUser = req.user;
   next();
 });
 
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-
-app.use("/user",userRoutes);
-app.use("/messages",messageRoutes);
+app.use("/user", userRoutes);
+app.use("/messages", messageRoutes);
 app.use("/", indexRoutes);
 //app.use('*',(req,res)=> res.redirect('/messaages'));
 
 app.use(passport.initialize());
-
 app.use(errorHandler);
 
 debug("App is ready to go");
 
-app.listen(port,()=> console.log(`Server started on port ${port}`));
+app.listen(port, () => debug(`Server started on port ${port}`));
--- config/db.js
+++ config/db.js
@@ -1,13 +1,14 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const debug = require("debug")("test");
 
-const connectDB = async()=>{
-  try{
+const connectDB = async () => {
+  try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`MongoDB connected: ${conn.connection.host}`);
-  }catch(error){
-    console.log(error);
+    debug(`MongoDB connected: ${conn.connection.host}`);
+  } catch (error) {
+    debug(error);
     process.exit(1);
   }
-}
+};
 
 module.exports = connectDB;
